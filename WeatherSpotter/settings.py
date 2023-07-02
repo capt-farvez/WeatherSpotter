50a1b3a2336f428b91e5613510ef173a12a004f1
Anowar Hossen Farvez <farvez165@gmail.com>
2023-07-03 00:22:43 +0600

    installed requests

--- WeatherSpotter/settings.py
+++ WeatherSpotter/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'WeatherSpotter.urls'
+#ROOT_URLCONF = 'home.urls'
 
 TEMPLATES = [
     {
